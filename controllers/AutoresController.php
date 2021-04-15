09a462bc841117a978e8b2f3d4d5445111c63dd6
Eduard2799 <eduardarauz2799@gmail.com>
2021-04-14 21:40:39 -0600

    Primer commit

--- controllers/AutoresController.php
+++ controllers/AutoresController.php
@@ -1,10 +1,20 @@
 <?php
 
   class AutoresController extends Controller {
+    
+    Static $libros_1 = [
+      ['books__book_id' => '1', 'books__title' => 'Operating System Concepts'],
+      ['books__book_id' => '2', 'books__title' => 'Database System Concepts']
+    ];
+
+    Static $libros_2 = [
+      ['books__book_id' => '3', 'books__title' => 'Computer Networks'],
+      ['books__book_id' => '4', 'books__title' => 'Modern Operating Systems']
+    ];
 
     static $autores_m = [
-      ['id' => '1', 'author' => 'Abraham Silberschatz', 'nationality' => 'Israelis / American', 'birth_year' => '1952', 'fields' => 'Database Systems, Operating Systems', 'books__book_id' => '1', 'books__title' => 'Operating System Concepts'],
-      ['id' => '2', 'author' => 'Andrew S. Tanenbaum', 'nationality' => 'Dutch / American', 'birth_year' => '1994', 'fields' => ' 	Distributed computing, Operating Systems', 'Computer Networks', 'books__title' => 'Operating System Concepts']
+      ['id' => '1', 'author' => 'Abraham Silberschatz', 'nationality' => 'Israelis / American', 'birth_year' => '1952', 'fields' => 'Database Systems, Operating Systems'],
+      ['id' => '2', 'author' => 'Andrew S. Tanenbaum', 'nationality' => 'Dutch / American', 'birth_year' => '1994', 'fields' => ' 	Distributed computing, Operating Systems', 'Computer Networks']
     ];
     
     public function index() {      
@@ -14,13 +24,20 @@
 
     public function show($id) { 
         $autor_m = [];
+        $libs_aut = [];
         foreach (self::$autores_m as $aut){
           if ($aut['id'] == $id){
             $autor_m = $aut;
+            if($aut['id'] == 1){
+              $libs_aut = self::$libros_1;
+            }
+            else if($aut['id'] == 2){
+              $libs_aut = self::$libros_2;
+            }
           }
         }
         
-        return view('Libros/Muestra', ['Autor_m' => $autor_m]);
+        return view('Autores/Muestra', ['autor_m' => $autor_m, 'libros_m' => $libs_aut]);
         }
     }
 
--- controllers/EditorialesController.php
+++ controllers/EditorialesController.php
@@ -2,6 +2,16 @@
 
   class EditorialesController extends Controller {
 
+    Static $libros_1 = [
+      ['books__book_id' => '1', 'books__title' => 'Operating System Concepts'],
+      ['books__book_id' => '2', 'books__title' => 'Database System Concepts']
+    ];
+
+    Static $libros_2 = [
+      ['books__book_id' => '3', 'books__title' => 'Computer Networks'],
+      ['books__book_id' => '4', 'books__title' => 'Modern Operating Systems']
+    ];
+
     static $editoriales_m = [
       ['id' => '1', 'publisher' => 'John Wiley & Sons', 'country' => 'United States', 'founded' => '1807', 'genere' => 'Academic'],
       ['id' => '2', 'publisher' => 'Pearson Education', 'country' => 'United Kingdom', 'founded' => '1844', 'genere' => 'Education']
@@ -13,14 +23,21 @@
     }
 
     public function show($id) { 
-      $libro_m = [];
-      foreach (self::$editoriales_m as $lib){
-        if ($lib['id'] == $id){
-          $libro_m = $lib;
+      $editorial_m = [];
+      $libs_edit = [];
+      foreach (self::$editoriales_m as $edit){
+        if ($edit['id'] == $id){
+          $editorial_m = $edit;
+          if($edit['id'] == 1){
+            $libs_edit = self::$libros_1;
+          }
+          else if($edit['id'] == 2){
+            $libs_edit = self::$libros_2;
+          }
         }
       }
       
-      return view('Editoriales/Muestra', ['editorial_m' => $libro_m]);
+      return view('Editoriales/Muestra', ['editorial_m' => $editorial_m, 'libros_m' => $libs_edit]);
       }
     }
 
